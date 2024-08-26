c21eec816f15d9f0940f996cafd2cb1c685d6759
VibhorTripathi <104970331+Vibhor9179@users.noreply.github.com>
2024-08-26 09:32:40 +0000

    Appr1

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,26 @@
 #include "stats.h"
+#include<cmath>
+using namespace std;
 
-Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
+Statistics::Stats Statistics::ComputeStatistics(const std::vector<float>& nums) {
     //Implement statistics here
+    Statistics::Stats ans;
+    ans.average = NAN;
+    ans.min = NAN;
+    ans.max = NAN;
+
+    if(nums.size())
+    {
+        float sum = 0;
+        for(auto it : nums)
+        {
+            sum += it;
+            ans.min = (ans.min < it)? ans.min : it;
+            ans.max = (ans.max > it)? ans.max : it;
+        }
+        ans.average = sum/nums.size();
+    }
+    return ans;
+
+
 }
